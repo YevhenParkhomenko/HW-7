a6d793740a6e154bdce026ed4befc802bbae02a0
YevhenParkhomenko <30552616+YevhenParkhomenko@users.noreply.github.com>
2021-11-21 23:39:52 +0200

    Update ConsoleLog.java

--- HW-7.5.2/src/com/yevhen_parkhomenko/main/ConsoleLog.java
+++ HW-7.5.2/src/com/yevhen_parkhomenko/main/ConsoleLog.java
@@ -1,6 +1,6 @@
 package com.yevhen_parkhomenko.main;
 
-public class ConsoleLog implements ILog {
+public class Log implements ILog {
     @Override
     public void print(Email email) {
         System.out.println("Email from '" + email.from + "' to '" + email.to + "' was send");
